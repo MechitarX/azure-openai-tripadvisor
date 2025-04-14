5e57f51febfb3b5f0044be6c5cb1c0b17402a0c8
Maciek Ciesla <maciej.m.ciesla@gmail.com>
2025-04-14 15:35:23 +0200

    set url

--- function_app.py
+++ function_app.py
@@ -5,8 +5,8 @@ import os
 
 app =func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="SecretGarden",route="")
-def SecretGarden(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="answernow")
+def answernow(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
 
