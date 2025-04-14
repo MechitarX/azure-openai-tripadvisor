bfd22e6c1a87850a4e1c91d8672d086fa5f473fe
Maciek Ciesla <maciej.m.ciesla@gmail.com>
2025-04-14 15:34:00 +0200

    change route

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ import os
 
 app =func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="SecretGarden")
+@app.route(route="SecretGarden",route="")
 def SecretGarden(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
